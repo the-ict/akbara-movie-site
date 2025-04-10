2cbac265a807bcdb063c413a4abb4a0426779e82
the-ict <dvltinv@gmail.com>
2025-04-10 21:34:21 +0500

    added some little singles

--- client/src/components/Marketing.tsx
+++ client/src/components/Marketing.tsx
@@ -4,7 +4,7 @@ export default function Marketing() {
   return (
     <div className="w-full xlmargin relative rounded overflow-hidden">
         <img src={Movies} alt="movies" className="w-full h-[300px] object-cover max-lg:bg-bottom-left max-lg:object-cover" />
-        <div className='absolute inset-0 bg-gradient-to-r from-black to-transparent'></div>
+        <div className='absolute inset-0 bg-gradient-to-r from-black to-transparent max-lg:bg-gradient-to-b max-lg:from-black max-lg:to-transparent'></div>
         <div className="absolute top-0 flex items-center justify-center h-full w-full">
            <div className="flex items-center w-[80%] justify-between max-lg:flex-col max-lg:gap-10 max-lg:text-center">
                 <div>
