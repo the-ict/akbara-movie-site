26574b8f0b9b62e25700a31e24a7e11cf6397150
the-ict <dvltinv@gmail.com>
2025-04-21 20:09:06 +0500

    added review style

--- client/src/components/WriteReview.tsx
+++ client/src/components/WriteReview.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import X from "../assets/icons/xred.png";
+import GapStar from "../assets/icons/gapstar.png";
 
 type Props = {
-  setAskQuestion: React.Dispatch<React.SetStateAction<boolean>>;
+  setReviewForm: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-export default function ({ setAskQuestion }: Props) {
+export default function ({ setReviewForm }: Props) {
   return (
     <div className="w-full h-screen fixed top-0 left-0 flex justify-center items-center z-10">
       <div className="flex flex-col gap-[10px] bg-[#0F0F0F] ask rounded w-[400px] relative">
@@ -13,18 +14,27 @@ export default function ({ setAskQuestion }: Props) {
           src={X}
           alt="x red png"
           className="w-[30px] h-[30px] object-contain cursor-pointer absolute top-2 right-2"
-          onClick={() => setAskQuestion(false)}
+          onClick={() => setReviewForm(false)}
         />
-        <label>Emailingiz</label>
+        <label>Izoh</label>
         <input
           type="text"
           className="border-[1px] border-white outline-none ask-input rounded"
         />
-        <label>Savolingiz</label>
+        <label>Davlat</label>
         <input
           type="text"
           className="border-[1px] border-white outline-none ask-input rounded"
         />
+        <label>Reyting</label>
+        <div className="flex items-center gap-[1rem]">
+          {
+            [1,2,3,4,5].map(item => (
+              <img src={GapStar} key={item} alt="" className="w-[30px] h-[30px] object-contain cursor-pointer" />
+            ))
+          }
+          
+        </div>
         <button className="cursor-pointer bg-[#e90000] ask-input font-bold">
           Yuborish!
         </button>
--- client/src/pages/SingleMedia.tsx
+++ client/src/pages/SingleMedia.tsx
@@ -1,239 +1,274 @@
 import Navbar from "../components/Navbar";
 
-import Play from "../assets/icons/play.png"
-import Add from "../assets/icons/add.png"
-import Like from "../assets/icons/like.png"
-import Sound from "../assets/icons/sound.png"
-import Date from "../assets/icons/date.png"
-import Language from "../assets/icons/language.png"
-import Rating from "../assets/icons/ratings.png"
-import Star from "../assets/icons/star.png"
-import HalfStar from "../assets/icons/halfstar.png"
-import Genres from "../assets/icons/genres.png"
-import AuthorProfile from "../assets/icons/authorprofile.png"
-import Plus from "../assets/icons/plus.png"
+import Play from "../assets/icons/play.png";
+import Add from "../assets/icons/add.png";
+import Like from "../assets/icons/like.png";
+import Sound from "../assets/icons/sound.png";
+import Date from "../assets/icons/date.png";
+import Language from "../assets/icons/language.png";
+import Rating from "../assets/icons/ratings.png";
+import Star from "../assets/icons/star.png";
+import HalfStar from "../assets/icons/halfstar.png";
+import Genres from "../assets/icons/genres.png";
+import AuthorProfile from "../assets/icons/authorprofile.png";
+import Plus from "../assets/icons/plus.png";
 import Marketing from "../components/Marketing";
 import Carousel from "react-multi-carousel";
 import { useEffect, useRef, useState } from "react";
 import Review from "../components/Review";
 
-
-import {useParams} from "react-router-dom"
+import { useParams } from "react-router-dom";
 import axios from "axios";
 import { IMovie } from "../types/Movie";
 import { getRatingValue } from "../functions/rating";
-
-
+import WriteReview from "../components/WriteReview";
 
 const responsive = {
   superLargeDesktop: {
     breakpoint: { max: 4000, min: 3000 },
-    items: 2
+    items: 2,
   },
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
-    items: 2
+    items: 2,
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
-    items: 2
+    items: 2,
   },
 };
 
 export default function SingleMedia() {
-  const [movie, setMovie] = useState<IMovie>()
-  const carouselRef = useRef<Carousel | null>(null)
+  const [movie, setMovie] = useState<IMovie>();
+  const [reviewForm, setReviewForm] = useState<boolean>(false);
+  const carouselRef = useRef<Carousel | null>(null);
 
-  const params = useParams()
-  
+  const params = useParams();
 
   useEffect(() => {
-    const getSingleMedia = async() => {
+    const getSingleMedia = async () => {
       try {
-        const response = await axios.get(`http://localhost:5000/api/movie/${params.id}`)
-        console.log(response.data)
-        setMovie(response.data)
+        const response = await axios.get(
+          `http://localhost:5000/api/movie/${params.id}`
+        );
+        console.log(response.data);
+        setMovie(response.data);
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
-    }
+    };
 
-    getSingleMedia()
-  }, [])
+    getSingleMedia();
+  }, []);
   return (
     <div>
-        <Navbar />
-        
-        {/* Header Section */}
-        <div className="relative w-full header">
-            <img 
-                src={movie?.thumbnail}
-                alt="movie cover"
-                className="w-full h-[calc(100vh-25vh)] object-cover"
-            />
-            <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent"></div>
-            
-            {/* Title and Play/Action Buttons */}
-            <div className="absolute bottom-20 w-full flex items-center justify-center">
-                <div className="w-[70%] text-center">
-                    <h3 className="text-3xl font-bold max-lg:text-2xl">{movie?.name}</h3>
+      <Navbar />
 
-                    <div className="flex items-center gap-5 header justify-center navbar max-lg:flex-col">
-                        <button className="flex items-center gap-2 font-bold cursor-pointer bg-[#E50000] play-button rounded">
-                            <img src={Play} alt="play icon" className="w-[20px] h-[20px] object-contain" />
-                            <span>Play now</span>
-                        </button>
-                        <div className="flex items-center gap-4">
-                          <button className="cursor-pointer bg-[#0F0F0F] button">
-                              <img src={Add} alt="add icon" className="w-[20px] h-[20px] object-contain" />
-                          </button>
-                          <button className="cursor-pointer bg-[#0F0F0F] button">
-                              <img src={Like} alt="like icon" className="w-[20px] h-[20px] object-contain" />
-                          </button>
-                          <button className="cursor-pointer bg-[#0F0F0F] button">
-                              <img src={Sound} alt="sound icon" className="w-[20px] h-[20px] object-contain" />
-                          </button>
-                        </div>
-                    </div>
-                </div>
+      {/* Header Section */}
+      <div className="relative w-full header">
+        <img
+          src={movie?.thumbnail}
+          alt="movie cover"
+          className="w-full h-[calc(100vh-25vh)] object-cover"
+        />
+        <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent"></div>
+
+        {/* Title and Play/Action Buttons */}
+        <div className="absolute bottom-20 w-full flex items-center justify-center">
+          <div className="w-[70%] text-center">
+            <h3 className="text-3xl font-bold max-lg:text-2xl">
+              {movie?.name}
+            </h3>
+
+            <div className="flex items-center gap-5 header justify-center navbar max-lg:flex-col">
+              <button className="flex items-center gap-2 font-bold cursor-pointer bg-[#E50000] play-button rounded">
+                <img
+                  src={Play}
+                  alt="play icon"
+                  className="w-[20px] h-[20px] object-contain"
+                />
+                <span>Play now</span>
+              </button>
+              <div className="flex items-center gap-4">
+                <button className="cursor-pointer bg-[#0F0F0F] button">
+                  <img
+                    src={Add}
+                    alt="add icon"
+                    className="w-[20px] h-[20px] object-contain"
+                  />
+                </button>
+                <button className="cursor-pointer bg-[#0F0F0F] button">
+                  <img
+                    src={Like}
+                    alt="like icon"
+                    className="w-[20px] h-[20px] object-contain"
+                  />
+                </button>
+                <button className="cursor-pointer bg-[#0F0F0F] button">
+                  <img
+                    src={Sound}
+                    alt="sound icon"
+                    className="w-[20px] h-[20px] object-contain"
+                  />
+                </button>
+              </div>
             </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Media Information Section */}
+      <div className="flex gap-5 justify-between max-lg:flex-col">
+        {/* Left Section: Description & Reviews */}
+        <div className="flex-7 flex flex-col gap-5">
+          <div className="single-cart bg-[#262626] rounded">
+            <p className="text-[#999999]">Description</p>
+            <p className="navbar">{movie?.description}</p>
+          </div>
+
+          {/* Reviews Section */}
+          <div className="single-cart bg-[#262626] max-lg:hidden">
+            <div className="flex items-center justify-between">
+              <h1 className="text-[#999999]">Reviews</h1>
+              <button className="flex items-center gap-5 button bg-[#141414] cursor-pointer rounded">
+                <img
+                  src={Plus}
+                  alt="plus"
+                  className="w-[30px] h-[30px] object-contain"
+                />
+                <span>Add Your Review</span>
+              </button>
+            </div>
+
+            {/* Review Cards */}
+            <div className="header">
+              <Review />
+            </div>
+          </div>
+
+          {/* Pagination */}
         </div>
 
-        {/* Media Information Section */}
-        <div className="flex gap-5 justify-between max-lg:flex-col">
-            {/* Left Section: Description & Reviews */}
-            <div className="flex-7 flex flex-col gap-5">
-                <div className="single-cart bg-[#262626] rounded">
-                    <p className="text-[#999999]">Description</p>
-                    <p className="navbar">{movie?.description}</p>
-                </div>
+        {/* Right Section: Media Details */}
+        <div className="flex-3 info flex flex-col gap-5 bg-[#262626] rounded single-cart-review">
+          <label>
+            <img src={Date} alt="date" />
+            <span>Kino chiqgan vaqt</span>
+          </label>
+          <h1>{movie?.created_time}</h1>
+          <label>
+            <img src={Language} alt="langauge" />
+            <span>Ko'rish mumkin bo'lgan tillar</span>
+          </label>
+          <div>
+            <span className="bg-[#141414] info-lang-cart">Uzbek</span>
+          </div>
 
-                {/* Reviews Section */}
-                <div className="single-cart bg-[#262626] max-lg:hidden">
-                    <div className="flex items-center justify-between">
-                        <h1 className="text-[#999999]">Reviews</h1>
-                        <button className="flex items-center gap-5 button bg-[#141414] cursor-pointer rounded">
-                            <img src={Plus} alt="plus" className="w-[30px] h-[30px] object-contain" />
-                            <span>Add Your Review</span>
-                        </button>
-                    </div>
+          <label>
+            <img src={Rating} alt="rating" />
+            <span>Ratings</span>
+          </label>
 
-                    {/* Review Cards */}
-                    <div className="header">
-                      
-                      <Review />
-                        
-                    </div>
-                </div>
-
-                {/* Pagination */}
-                
-            </div>
-
-            {/* Right Section: Media Details */}
-            <div className="flex-3 info flex flex-col gap-5 bg-[#262626] rounded single-cart-review">
-              <label>
-                <img src={Date} alt="date"/>
-                <span>Kino chiqgan vaqt</span>
-              </label>
-              <h1>{movie?.created_time}</h1>
-              <label>
-                <img src={Language} alt="langauge" />
-                <span>Ko'rish mumkin bo'lgan tillar</span>
-              </label>
-              <div>
-                <span className="bg-[#141414] info-lang-cart">Uzbek</span>
-              </div>
-              
-              <label >
-                <img src={Rating} alt="rating" />
-                <span>Ratings</span>
-              </label>
-              
-              <div className="flex items-center gap-5">
-                <div className="font-bold button bg-[#141414] flex-1/2" onClick={() => {
-                  window.location.replace(String(movie?.ratings[0].link))
-                }}>
-                  <p>IMDB</p>
-                  <div className="flex items-center gap-1 star-container cursor-pointer">
-                    {
-                      movie?.ratings[0].rating && (
-                        getRatingValue(Number(movie?.ratings[0].rating)).map((item) => {
-                          if(item == 1) {
-                            return (
-                              <img src={Star} alt="star"  className="max-lg:w-[10px] max-lg:h-[10px]" key={item + Math.random()}/>
-                            )
-                          }
-                          return (
-                              <img src={HalfStar} alt="half-star" className="max-lg:w-[10px] max-lg:h-[10px]" key={item + Math.random()}/>
-                          )
-                      })
-                      )
-                    }
-                    <span>{movie?.ratings[0].rating}</span>
-                  </div>
-                </div>
-
-              </div>
-
-              <label>
-                <img src={Genres} alt="" />
-                <span>Gernes</span>
-              </label>
-              <div>
-                {
-                  movie?.Genres && (
-                    movie.Genres.map((genre, index) => {
-                      console.log(genre)
+          <div className="flex items-center gap-5">
+            <div
+              className="font-bold button bg-[#141414] flex-1/2"
+              onClick={() => {
+                window.location.replace(String(movie?.ratings[0].link));
+              }}
+            >
+              <p>IMDB</p>
+              <div className="flex items-center gap-1 star-container cursor-pointer">
+                {movie?.ratings[0].rating &&
+                  getRatingValue(Number(movie?.ratings[0].rating)).map(
+                    (item) => {
+                      if (item == 1) {
+                        return (
+                          <img
+                            src={Star}
+                            alt="star"
+                            className="max-lg:w-[10px] max-lg:h-[10px]"
+                            key={item + Math.random()}
+                          />
+                        );
+                      }
                       return (
-                        <span key={index}>{genre}</span>
+                        <img
+                          src={HalfStar}
+                          alt="half-star"
+                          className="max-lg:w-[10px] max-lg:h-[10px]"
+                          key={item + Math.random()}
+                        />
                       );
-                    })
-                  )
-                }
-              </div>
-
-              <label>
-                Director
-              </label>
-              <div className="flex items-center gap-5 button bg-[#141414] rounded">
-                <img src={AuthorProfile} alt="authorprofile" className="h-full w-[20%] object-contain"/>
-                <div>
-                  <h1 className="font-bold">{movie?.Director[0]?.name}</h1>
-                  <p>From {movie?.Director[0]?.country}</p>
-                </div>
-              </div>
-
-              <label>
-                Music
-              </label>
-              <div className="flex items-center gap-5 button bg-[#141414] rounded">
-                <img src={AuthorProfile} alt="authorprofile" className="h-full w-[20%] object-contain"/>
-                <div>
-                  <h1 className="font-bold">{movie?.Music[0]?.name}</h1>
-                  <p>From {movie?.Music[0]?.country}</p>
-                </div>
+                    }
+                  )}
+                <span>{movie?.ratings[0].rating}</span>
               </div>
             </div>
+          </div>
 
-            <div className="arrow-padding bg-[#262626] lg:hidden">
-                <div className="flex items-center justify-between">
-                    <h1 className="text-[#999999]">Reviews</h1>
-                    <button className="flex items-center gap-5 button bg-[#141414] cursor-pointer rounded">
-                        <img src={Plus} alt="plus" className="w-[30px] h-[30px] object-contain" />
-                        <span>Add Your Review</span>
-                    </button>
-                </div>
+          <label>
+            <img src={Genres} alt="" />
+            <span>Gernes</span>
+          </label>
+          <div>
+            {movie?.Genres &&
+              movie.Genres.map((genre, index) => {
+                console.log(genre);
+                return <span key={index}>{genre}</span>;
+              })}
+          </div>
 
-                {/* Review Cards */}
-                <div className="header">
-                  
-                  <Review />
-                    
-                </div>
+          <label>Director</label>
+          <div className="flex items-center gap-5 button bg-[#141414] rounded">
+            <img
+              src={AuthorProfile}
+              alt="authorprofile"
+              className="h-full w-[20%] object-contain"
+            />
+            <div>
+              <h1 className="font-bold">{movie?.Director[0]?.name}</h1>
+              <p>From {movie?.Director[0]?.country}</p>
             </div>
+          </div>
+
+          <label>Music</label>
+          <div className="flex items-center gap-5 button bg-[#141414] rounded">
+            <img
+              src={AuthorProfile}
+              alt="authorprofile"
+              className="h-full w-[20%] object-contain"
+            />
+            <div>
+              <h1 className="font-bold">{movie?.Music[0]?.name}</h1>
+              <p>From {movie?.Music[0]?.country}</p>
+            </div>
+          </div>
         </div>
-        <Marketing />
+
+        {reviewForm && <WriteReview setReviewForm={setReviewForm} />}
+        <div className="arrow-padding bg-[#262626] lg:hidden">
+
+          <div className="flex items-center justify-between">
+            <h1 className="text-[#999999]">Reviews</h1>
+            <button 
+              onClick={() => setReviewForm(true)}
+              className="flex items-center gap-5 button bg-[#141414] cursor-pointer rounded">
+              <img
+                src={Plus}
+                alt="plus"
+                className="w-[30px] h-[30px] object-contain"
+              />
+              <span>Add Your Review</span>
+            </button>
+          </div>
+
+          {/* Review Cards */}
+          <div className="header">
+            <Review />
+          </div>
+        </div>
+      </div>
+      <Marketing />
     </div>
   );
 }
