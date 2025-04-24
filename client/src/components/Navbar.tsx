376d411ffd2e0003515cebc6d33d85fac01e4fe4
the-ict <dvltinv@gmail.com>
2025-04-24 16:23:48 +0500

    latest

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -5,7 +5,7 @@ import X from "../assets/icons/xred.png";
 
 import "../styles.css";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FaArrowAltCircleDown } from "react-icons/fa";
 
 export default function Navbar() {
@@ -34,41 +34,133 @@ export default function Navbar() {
           Bosh sahifa
         </Link>
         <div className="relative group flex items-center gap-2 cursor-pointer hover:bg-[#1A1A1A] navigation-item">
-          <p className="cursor-pointer relative rounded">
-            Kategoriyalar
-          </p>
+          <p className="cursor-pointer relative rounded">Kategoriyalar</p>
           <FaArrowAltCircleDown />
           {/* hidden category menu */}
           <div className="absolute top-full left-0 z-10 hidden w-full bg-[#1A1A1A] group-hover:block">
             <div className="footer arrow-padding w-full">
               <div className="flex justify-between footer-nav flex-wrap gap-5">
-                <ul className="max-lg:w-[45%]">
-                  <li className="footer-item">Drama</li>
-                  <li>Komediya</li>
-                  <li>Triller</li>
-                  <li>Dahshat</li>
-                  <li>Fantastika</li>
+                <ul className="max-lg:w-[45%] flex flex-col">
+                  <Link
+                    to={`/search?genre=Drama`}
+                    className="footer-item hover:underline font-bold"
+                  >
+                    Drama
+                  </Link>
+                  <Link
+                    to={`/search?genre=Komediya`}
+                    className="hover:underline"
+                  >
+                    Komediya
+                  </Link>
+                  <Link
+                    to={`/search?genre=Triller`}
+                    className="hover:underline"
+                  >
+                    Triller
+                  </Link>
+                  <Link
+                    to={`/search?genre=Dahshat`}
+                    className="hover:underline"
+                  >
+                    Dahshat
+                  </Link>
+                  <Link
+                    to={`/search?genre=Fantastika`}
+                    className="hover:underline"
+                  >
+                    Fantastika
+                  </Link>
                 </ul>
-                <ul className="max-lg:w-[45%]">
-                  <li className="footer-item">Fantaziya</li>
-                  <li>Jangari</li>
-                  <li>Sarguzasht</li>
-                  <li>Detektiv</li>
-                  <li>Romantik</li>
+                <ul className="max-lg:w-[45%] flex flex-col">
+                  <Link
+                    to={`/search?genre=Fantaziya`}
+                    className="footer-item hover:underline font-bold"
+                  >
+                    Fantaziya
+                  </Link>
+                  <Link
+                    to={`/search?genre=Jangari`}
+                    className="hover:underline"
+                  >
+                    Jangari
+                  </Link>
+                  <Link
+                    to={`/search?genre=Sarguzasht`}
+                    className="hover:underline"
+                  >
+                    Sarguzasht
+                  </Link>
+                  <Link
+                    to={`/search?genre=Detektiv`}
+                    className="hover:underline"
+                  >
+                    Detektiv
+                  </Link>
+                  <Link
+                    to={`/search?genre=Romantik`}
+                    className="hover:underline"
+                  >
+                    Romantik
+                  </Link>
                 </ul>
-                <ul className="max-lg:w-[45%]">
-                  <li className="footer-item">Tarixiy</li>
-                  <li>Melodrama</li>
-                  <li>Urush</li>
-                  <li>Vestern</li>
-                  <li>Noir</li>
+                <ul className="max-lg:w-[45%] flex flex-col">
+                  <Link
+                    to={`/search?genre=Tarixiy`}
+                    className="footer-item hover:underline font-bold"
+                  >
+                    Tarixiy
+                  </Link>
+                  <Link
+                    to={`/search?genre=Melodrama`}
+                    className="hover:underline"
+                  >
+                    Melodrama
+                  </Link>
+                  <Link to={`/search?genre=Urush`} className="hover:underline">
+                    Urush
+                  </Link>
+                  <Link
+                    to={`/search?genre=Vestern`}
+                    className="hover:underline"
+                  >
+                    Vestern
+                  </Link>
+                  <Link to={`/search?genre=Noir`} className="hover:underline">
+                    Noir
+                  </Link>
                 </ul>
-                <ul className="max-lg:w-[45%]">
-                  <li className="footer-item">Kriminal</li>
-                  <li>Musobaqali</li>
-                  <li>Oilaviy</li>
-                  <li>Musiqiy</li>
-                  <li>Psixologik drama</li>
+                <ul className="max-lg:w-[45%] flex flex-col">
+                  <Link
+                    to={`/search?genre=Kriminal`}
+                    className="footer-item hover:underline font-bold"
+                  >
+                    Kriminal
+                  </Link>
+                  <Link
+                    to={`/search?genre=Musobaqali`}
+                    className="hover:underline"
+                  >
+                    Musobaqali
+                  </Link>
+                  <Link
+                    to={`/search?genre=Oilaviy`}
+                    className="hover:underline"
+                  >
+                    Oilaviy
+                  </Link>
+                  <Link
+                    to={`/search?genre=Musiqiy`}
+                    className="hover:underline"
+                  >
+                    Musiqiy
+                  </Link>
+                  <Link
+                    to={`/search?genre=Psixologik drama`}
+                    className="hover:underline"
+                  >
+                    Psixologik drama
+                  </Link>
                 </ul>
               </div>
             </div>
