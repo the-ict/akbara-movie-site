1d95f26ff50fd79283f2cf7ac453b862e2126f48
the-ict <dvltinv@gmail.com>
2025-04-25 12:55:27 +0500

    commited latest

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,31 +1,66 @@
+import { useState, useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+import { FaArrowAltCircleDown } from "react-icons/fa";
+
 import Logo from "../assets/images/logo.png";
 import Search from "../assets/icons/search.png";
 import Menu from "../assets/icons/menu.png";
 import X from "../assets/icons/xred.png";
 
 import "../styles.css";
-import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { FaArrowAltCircleDown } from "react-icons/fa";
 
 export default function Navbar() {
-  const [search, setSearch] = useState<boolean>(false);
   const [searchInput, setSearchInput] = useState<string>("");
   const [hiddenMenu, setHiddenMenu] = useState<boolean>(false);
+  const [categoryHiddenMenu, setCategoryHiddenMenu] = useState<boolean>(false);
+  const [hiddenYear, setHiddenYear] = useState<boolean>(false);
+  const [hiddenGenre, setHiddenGenre] = useState<boolean>(false);
+  const [hiddenCountry, setHiddenCountry] = useState<boolean>(false);
+
+  const categoryRef = useRef<HTMLDivElement | null>(null);
+  const yearRef = useRef<HTMLDivElement | null>(null);
+  const genreRef = useRef<HTMLDivElement | null>(null);
+  const countryRef = useRef<HTMLDivElement | null>(null);
+
+  // Close menus on outside click
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        categoryRef.current &&
+        !categoryRef.current.contains(event.target as Node)
+      ) {
+        setCategoryHiddenMenu(false);
+      }
+      if (yearRef.current && !yearRef.current.contains(event.target as Node)) {
+        setHiddenYear(false);
+      }
+      if (
+        genreRef.current &&
+        !genreRef.current.contains(event.target as Node)
+      ) {
+        setHiddenGenre(false);
+      }
+      if (
+        countryRef.current &&
+        !countryRef.current.contains(event.target as Node)
+      ) {
+        setHiddenCountry(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
     <div className="flex items-center justify-between navbar-padding relative">
       <a href="/">
-        {/* <img
-          src={Logo}
-          alt="logo"
-          className="w-[50px] max-lg:w-[50px] h-full object-contain cursor-pointer"
-        /> */}
-
         <span className="text-2xl font-bold">
           ATV<span className="text-green-600">.</span>uz
         </span>
       </a>
+
       <ul className="flex items-center gap-5 bg-[#0F0F0F] navigation rounded max-lg:hidden">
         <Link
           className="cursor-pointer navigation-item rounded hover:bg-[#1A1A1A]"
@@ -33,138 +68,115 @@ export default function Navbar() {
         >
           Bosh sahifa
         </Link>
-        <div className="relative group flex items-center gap-2 cursor-pointer hover:bg-[#1A1A1A] navigation-item">
-          <p className="cursor-pointer relative rounded">Kategoriyalar</p>
+
+        {/* Kategoriyalar */}
+        <div
+          ref={categoryRef}
+          className="flex items-center relative gap-2 cursor-pointer hover:bg-[#1A1A1A] navigation-item"
+        >
+          <p
+            className="cursor-pointer relative rounded"
+            onClick={() => setCategoryHiddenMenu(!categoryHiddenMenu)}
+          >
+            Kategoriyalar
+          </p>
           <FaArrowAltCircleDown />
-          {/* hidden category menu */}
-          <div className="absolute top-full left-0 z-10 hidden w-full bg-[#1A1A1A] group-hover:block">
-            <div className="footer arrow-padding w-full">
-              <div className="flex justify-between footer-nav flex-wrap gap-5">
-                <ul className="max-lg:w-[45%] flex flex-col">
-                  <Link
-                    to={`/search?genre=Drama`}
-                    className="footer-item hover:underline font-bold"
-                  >
-                    Drama
-                  </Link>
-                  <Link
-                    to={`/search?genre=Komediya`}
-                    className="hover:underline"
-                  >
-                    Komediya
-                  </Link>
-                  <Link
-                    to={`/search?genre=Triller`}
-                    className="hover:underline"
-                  >
-                    Triller
-                  </Link>
-                  <Link
-                    to={`/search?genre=Dahshat`}
-                    className="hover:underline"
-                  >
-                    Dahshat
-                  </Link>
-                  <Link
-                    to={`/search?genre=Fantastika`}
-                    className="hover:underline"
-                  >
-                    Fantastika
-                  </Link>
-                </ul>
-                <ul className="max-lg:w-[45%] flex flex-col">
-                  <Link
-                    to={`/search?genre=Fantaziya`}
-                    className="footer-item hover:underline font-bold"
-                  >
-                    Fantaziya
-                  </Link>
-                  <Link
-                    to={`/search?genre=Jangari`}
-                    className="hover:underline"
-                  >
-                    Jangari
-                  </Link>
-                  <Link
-                    to={`/search?genre=Sarguzasht`}
-                    className="hover:underline"
-                  >
-                    Sarguzasht
-                  </Link>
-                  <Link
-                    to={`/search?genre=Detektiv`}
-                    className="hover:underline"
-                  >
-                    Detektiv
-                  </Link>
-                  <Link
-                    to={`/search?genre=Romantik`}
-                    className="hover:underline"
-                  >
-                    Romantik
-                  </Link>
-                </ul>
-                <ul className="max-lg:w-[45%] flex flex-col">
-                  <Link
-                    to={`/search?genre=Tarixiy`}
-                    className="footer-item hover:underline font-bold"
-                  >
-                    Tarixiy
-                  </Link>
-                  <Link
-                    to={`/search?genre=Melodrama`}
-                    className="hover:underline"
-                  >
-                    Melodrama
-                  </Link>
-                  <Link to={`/search?genre=Urush`} className="hover:underline">
-                    Urush
-                  </Link>
-                  <Link
-                    to={`/search?genre=Vestern`}
-                    className="hover:underline"
-                  >
-                    Vestern
-                  </Link>
-                  <Link to={`/search?genre=Noir`} className="hover:underline">
-                    Noir
-                  </Link>
-                </ul>
-                <ul className="max-lg:w-[45%] flex flex-col">
-                  <Link
-                    to={`/search?genre=Kriminal`}
-                    className="footer-item hover:underline font-bold"
-                  >
-                    Kriminal
-                  </Link>
-                  <Link
-                    to={`/search?genre=Musobaqali`}
-                    className="hover:underline"
-                  >
-                    Musobaqali
-                  </Link>
-                  <Link
-                    to={`/search?genre=Oilaviy`}
-                    className="hover:underline"
-                  >
-                    Oilaviy
-                  </Link>
-                  <Link
-                    to={`/search?genre=Musiqiy`}
-                    className="hover:underline"
-                  >
-                    Musiqiy
-                  </Link>
-                  <Link
-                    to={`/search?genre=Psixologik drama`}
-                    className="hover:underline"
-                  >
-                    Psixologik drama
-                  </Link>
-                </ul>
-              </div>
+          {categoryHiddenMenu && (
+            <div className="absolute flex flex-col gap-2 top-10 z-10 bg-[#1A1A1A] navbar-padding">
+              <p
+                className="font-bold hover:underline"
+                onClick={() => {
+                  setHiddenYear(!hiddenYear);
+                  setHiddenCountry(false);
+                  setHiddenGenre(false);
+                }}
+              >
+                Yil bo'yicha
+              </p>
+              <p
+                className="font-bold hover:underline"
+                onClick={() => {
+                  setHiddenYear(false);
+                  setHiddenCountry(!hiddenCountry);
+                  setHiddenGenre(false);
+                }}
+              >
+                Davlat bo'yicha
+              </p>
+              <p
+                className="font-bold hover:underline"
+                onClick={() => {
+                  setHiddenYear(false);
+                  setHiddenCountry(false);
+                  setHiddenGenre(!hiddenGenre);
+                }}
+              >
+                Janrlar bo'yicha
+              </p>
+              {/* Year hidden menu */}
+              {hiddenYear && (
+                <div className="absolute left-full z-10 bg-[#1A1A1A] navbar-padding">
+                  <div className="flex flex-col gap-5">
+                    {[
+                      2010, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019,
+                      2020, 2021, 2022, 2023, 2024, 2025,
+                    ].map((item, _) => (
+                      <span key={_}>{item}</span>
+                    ))}
+                  </div>
+                </div>
+              )}
+              {/* Country hidden menu */}
+              {hiddenCountry && (
+                <div className="absolute left-full z-10 bg-[#1A1A1A] navbar-padding">
+                  <div className="flex flex-col gap-3">
+                    {["Uzbekistan", "Aqsh", "Arab", "Tojik", "Hind"].map(
+                      (item, _) => (
+                        <span key={_}>{item}</span>
+                      )
+                    )}
+                  </div>
+                </div>
+              )}
+              {/* Genre hidden menu */}
+              {hiddenGenre && (
+                <div className="absolute left-full z-10 bg-[#1A1A1A] navbar-padding">
+                  <div className="flex flex-col gap-5">
+                    {[
+                      "Drama",
+                      "Komediya",
+                      "Triller",
+                      "Dahshat",
+                      "Fantastika",
+                      "Fantaziya",
+                      "Jangari",
+                      "Sarguzasht",
+                      "Detektiv",
+                      "Romantik",
+                      "Tarixiy",
+                      "Melodrama",
+                      "Urush",
+                      "Vestern",
+                      "Noir",
+                      "Kriminal",
+                      "Musobaqali",
+                      "Oilaviy",
+                      "Musiqiy",
+                      "Psixologik drama",
+                    ].map((item, _) => (
+                      <Link
+                        key={_}
+                        to={`/search?genre=${item}`}
+                        className="hover:underline"
+                      >
+                        {item}
+                      </Link>
+                    ))}
+                  </div>
+                </div>
+              )}
             </div>
-          </div>
+          )}
         </div>
 
         <Link
@@ -174,14 +186,14 @@ export default function Navbar() {
           Qo'llab quvatlash
         </Link>
       </ul>
+
+      {/* Search input */}
       <div className="flex items-center gap-7 max-lg:hidden">
         <div className="flex items-center gap-2">
           <input
             type="text"
             value={searchInput}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setSearchInput(e.target.value)
-            }
+            onChange={(e) => setSearchInput(e.target.value)}
             placeholder="Searching...."
             className="search"
           />
@@ -195,6 +207,8 @@ export default function Navbar() {
           />
         </div>
       </div>
+
+      {/* Hamburger menu (mobile) */}
       <img
         src={Menu}
         alt=""
@@ -202,13 +216,13 @@ export default function Navbar() {
         className="navigation rounded w-[50px] h-[50px] cursor-pointer lg:hidden"
       />
 
-      {/* Hidden menu */}
+      {/* Hidden mobile menu */}
       {hiddenMenu && (
         <div className="navbar-hidden-menu fixed top-0 z-10 h-screen max-sm:w-full max-lg:w-[calc(100vw-60vw)] bg-black/80 flex flex-col gap-10 justify-center text-white">
           <img
             src={X}
             className="absolute w-[30px] h-[30px] cursor-pointer top-2 right-2 max-sm:right-5"
-            alt=""
+            alt="close"
             onClick={() => setHiddenMenu(false)}
           />
           <a href="/">
@@ -233,9 +247,7 @@ export default function Navbar() {
             <input
               type="text"
               value={searchInput}
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                setSearchInput(e.target.value)
-              }
+              onChange={(e) => setSearchInput(e.target.value)}
               placeholder="Searching...."
               className="flex-1 search"
             />
